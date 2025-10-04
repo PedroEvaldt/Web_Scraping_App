3031eafe597fedda02f75511fd7ffe4e8c94a5dc
Pedro Evaldt <pedrofossati067@gmail.com>
2025-10-04 20:13:11 -0300

    Make requirements.txt

--- main.py
+++ main.py
@@ -1,6 +1,21 @@
-def main():
-    print("Hello from web-scraping-app!")
+# 1. User enter Amazon product ASIN
 
+# 2. Scrape product details from Amazon
 
-if __name__ == "__main__":
-    main()
+# 3. Store procut in local database
+
+# 4. User clicks "Start competitor analysis"
+
+# 5. Search for similar procuts on Amazon
+
+# 6. Scrape details for all competitors found
+
+# 7. Store competitors linked original product
+
+# 8. User clicks "Analyze with LLM"
+
+# 9. Sendo product + competitors to AI
+
+# 10. Get competitive analysis report
+
+# 11. Display results to user
